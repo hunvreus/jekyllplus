47608a00f9b302559f71ed0c812e2bdea06f09ad
Ronan Berder <hunvreus@gmail.com>
2018-02-26 17:59:04 +0800

    Working on saving

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,12 +1,16 @@
 <template>
   <div class='editor'>
-    <form v-if='config && config.collections && config.collections.posts' class='editor'>
+    <form v-if='config && config.collections && config.collections.posts' v-on:submit.prevent='save'>
       <header class='header'>
+        <div class='meta'>
+          <code class='file'>{{ this.path }}</code>
+          <div class='updated' v-if='history[0]'>Last updated {{ history[0].commit.author.date | fromNow }}</div>
+        </div>
         <!-- <history :commits='history'></history> -->
-        <button class='button primary small'>Save</button>
+        <button class='button primary' v-bind:class='{ disabled: status != "" }'>Save</button>
       </header>
       <div class='body'>
-        <field v-for='field in config.collections.posts.fields' :key='field.name' :field='field' :file='file'></field>
+        <field v-for='field in config.collections.posts.fields' :key='field.name' :field='field' :file='file' :form='form'></field>
       </div>
     </form>
   </div>
@@ -27,7 +31,10 @@ export default {
       config: {},
       file: {},
       history: {},
-      error: ''
+      error: '',
+      sha: '',
+      form: {},
+      status: ''
     };
   },
   components: {
@@ -48,10 +55,10 @@ export default {
     getFile: function () {
       this.path = this.$route.params.path;
       var url = 'https://api.github.com/repos/' + this.username + '/' + this.repo + '/contents/' + this.path + '?access_token=' + this.token;
-      this.$http.get(url, {headers: {'Accept': 'application/vnd.github.v3.raw'}}).then(response => {
+      this.$http.get(url).then(response => {
         this.error = '';
-        this.file = jsyaml.loadFront(response.body);
-        this.file.body = this.file.__content;
+        this.file = jsyaml.loadFront(window.atob(response.body.content), 'body');
+        this.sha = response.body.sha;
       }, response => {
         this.error = 'Error: ' + response.body.message;
       });
@@ -61,6 +68,7 @@ export default {
       this.$http.get(url, {headers: {'Accept': 'application/vnd.github.v3.raw'}}).then(response => {
         this.error = '';
         this.config = YAML.safeLoad(response.body);
+        this.config['__content'] = this.config.body;
       }, response => {
         this.error = 'Couldn\'t retrieve the configuration file (.jekyllplus.yml): ' + response.body.message;
       });
@@ -72,6 +80,34 @@ export default {
         this.history = response.body;
       }, response => {
         this.error = 'Couldn\'t retrieve the commit history: ' + response.body.message;
+      });
+    },
+    save: function (e) {
+      // Prepare the file content
+      var body = this.file.body;
+      var yaml = this.file;
+      delete yaml.body;
+      yaml = YAML.safeDump(yaml);
+      var content = '---\n';
+          content += yaml;
+          content += '---\n';
+          content += body;
+
+      var url = 'https://api.github.com/repos/' + this.username + '/' + this.repo + '/contents/' + this.path + '?access_token=' + this.token;
+      var params = {
+        path: this.path,
+        message: 'File edited through Jekyll+',
+        content: window.btoa(content),
+        sha: this.sha
+      };
+      this.status = 'saving';
+      this.$http.put(url, params).then(response => {
+        console.log();
+        this.getFile();
+        this.getHistory();
+        this.status = '';
+      }, response => {
+        this.error = 'Couldn\'t save the file: ' + response.body.message;
       });
     }
   }
--- src/components/Field.vue
+++ src/components/Field.vue
@@ -3,41 +3,42 @@
     <label>{{ field.label }}</label>
     <!-- String -->
     <div v-if='field.type == "string"'>
-      <input v-if='field.multiple' v-for='n in count' type='text' :value='value ? value[n - 1] : ""' :name='name + "[]"'/>
-      <input v-else type='text' :value='value' :name='name'/>
+      <input type='text' v-if='field.multiple' v-for='n in count' v-model='file[field.name][n - 1]'/>
+      <input type='text' v-else v-model='file[field.name]'/>
     </div>
     <!-- Switch -->
     <div v-if='field.type == "toggle"'>
-      <input v-if='field.multiple' v-for='n in count' type='checkbox' :value='value[n - 1] ? "on" : ""' :name='name'>
-      <input v-else type='checkbox' :value='value ? "on" : ""' :name='name'>
+      <input type='checkbox' v-if='field.multiple' v-for='n in count' v-model='file[field.name][n - 1]'/>
+      <input type='checkbox' v-else v-model='file[field.name]'/>
     </div>
     <!-- Date -->
     <div v-else-if='field.type == "date"'>
-      <input v-if='field.multiple' v-for='n in count' type='date' :value='value[n - 1]' :name='name + "[]"'/>
-      <input v-else type='date' :value='value' :name='name'/>
+      <input type='date' v-if='field.multiple' v-for='n in count' v-model='file[field.name][n - 1]'/>
+      <input type='date' v-else v-model='file[field.name]'/>
     </div>
     <!-- List -->
-    <div v-else-if='field.type == "list"'>
+    <div v-else-if='field.type == "select"'>
       <span class='select' v-if='field.multiple' v-for='n in count'>
-        <select :name='name + "[]"'>
-          <option v-for='option in field.values' :value='(typeof option == "string") ? option : option.value'>{{ (typeof option == "string") ? option : option.label }}</option>
+        <select v-model='file[field.name][n - 1]'>
+          <option v-for='option in field.options' :value='(typeof option == "string") ? option : option.value'>{{ (typeof option == "string") ? option : option.label }}</option>
         </select>
       </span>
       <span class='select' v-else>
-        <select :name='name'>
-          <option v-for='option in field.values' :value='(typeof option == "string") ? option : option.value'>{{ (typeof option == "string") ? option : option.label }}</option>
+        <select v-model='file[field.name]'>
+          <option v-for='option in field.options' :value='(typeof option == "string") ? option : option.value'>{{ (typeof option == "string") ? option : option.label }}</option>
         </select>
       </span>
     </div>
     <!-- Object -->
     <div v-else-if='field.type == "object"'>
       <fieldset>
-        <field v-for='field in field.fields' :key='field.name' :field='field' :file='value' :prefix='name'></field>
+        <field v-for='objField in field.fields' :key='objField.name' :field='objField' :file='file[field.name]'></field>
       </fieldset>
     </div>
+    <!-- Text -->
     <div v-else-if='field.type == "text" || field.type=="markdown"'>
-      <textarea v-if='field.multiple' v-for='n in count' :value='value[n - 1]' rows='20' :name='name + "[]"'/>
-      <textarea v-else :value='value' rows='20' :name='name'/>
+      <textarea rows='20' v-if='field.multiple' v-for='n in count' v-model='file[field.name][n - 1]'/>
+      <textarea rows='20' v-else v-model='file[field.name]'/>
     </div>
     <a v-if='field.multiple' v-on:click='count += 1' class='add'>Add an entry</a>
   </div>
@@ -46,7 +47,7 @@
 <script>
   export default {
     name: 'field',
-    props: [ 'field', 'file', 'prefix' ],
+    props: ['field', 'file', 'prefix'],
     data: function () {
       return {
         count: 1
@@ -58,9 +59,6 @@
       }
     },
     computed: {
-      name: function () {
-        return this.prefix ? this.prefix + '[' + this.field.name + ']' : this.field.name;
-      },
       value: function () {
         return (this.file && this.field && this.field.name) ? this.file[this.field.name] : '';
       }
