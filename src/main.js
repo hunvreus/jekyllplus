a736bb2efcfde6c1932ce6c006b7e9b172e686a8
Ronan Berder <hunvreus@gmail.com>
2018-03-27 15:00:46 +0800

    Attempting to use 404

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ import Editor from './components/Editor.vue'
 import Login from './components/Login.vue'
 
 const router = new VueRouter({
-  mode: 'history',
+  // mode: 'history',
   base: __dirname,
   routes: [
     {
